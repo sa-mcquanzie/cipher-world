1e1a5dd5bdd5bea458977ef9b6363a167833160d
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-12 16:44:27 +0100

    Added cipher.rb and spec for part 2

--- part_01/lib/cipher.rb
+++ part_01/lib/cipher.rb
@@ -1,4 +1,4 @@
 def encrypt (str, n)
-  dict = (('a'..'z').to_a.prepend(" ").zip((0..27).to_a.rotate(n))).to_h
+  dict = (('a'..'z').to_a.prepend(" ").zip((0..26).to_a.rotate(n))).to_h
   return str.chars.map { |char| dict[char] }.join
 end
