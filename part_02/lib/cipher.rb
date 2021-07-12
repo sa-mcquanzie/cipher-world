bd34ebd55d4b59d7ce81faf1aa71397dd2ba6c24
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-12 17:27:29 +0100

    Added encrypt method to part 2 Secret class

--- part_02/lib/cipher.rb
+++ part_02/lib/cipher.rb
@@ -1,3 +1,10 @@
 class Secret
-  
+  def initialize str
+    @str = str
+  end
+
+  def encrypt num
+    dict = (([' '] + ('a'..'z').to_a + ('A'..'Z').to_a + ['!']).zip((0..53).to_a.map { |n| n + num })).to_h
+    return @str.chars.map { |char| dict[char] }.join
+  end
 end
--- part_02/spec/cipher_spec.rb
+++ part_02/spec/cipher_spec.rb
@@ -1,9 +1,25 @@
 require 'cipher'
 
 describe 'Secret' do
-  it 'should exist' do
+  it 'should exist and take one argument' do
     Kernel.const_defined?('Secret')
   end
+
+  before(:each) do 
+    @secret = Secret.new('My name is Edward!') 
+  end
+
+  describe '#encrypt' do
+    context 'When instantiated with "My name is Edward!"' do
+      it 'should return the correct value when passed "3"' do
+        expect( @secret.encrypt 3 ).to eq '4228317416831222334726421756'
+      end
+
+      it 'should return the correct value when passed "7"' do
+        expect( @secret.encrypt 7 ).to eq '4632721820127162673811308251160'
+      end
+    end
+  end 
 end
 
 # Acceptance Criteria
