d64abebdb9c1e03a53d72d3538a9b6a7b0932861
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-13 17:30:46 +0100

    Part 4: Reused secret class from part 3

--- part_04/lib/cipher.rb
+++ part_04/lib/cipher.rb
@@ -1,6 +1,18 @@
 class Secret
   def initialize plaintext
     @plaintext = plaintext
+    @charset = {}
+
+    File.open('lib/character_set1.txt', 'r') do |f|
+      f.readlines.map(&:chomp).each { |str| @charset[str[0]] = str[str.rindex(",") + 2..-1] }
+    end
+  end
+
+  def encrypt int
+    @plaintext.chars.map do |char| 
+      "#{ (@charset[char].to_i + int) % 99 }"
+      .sub(/^[0-9]{1}$/) { |num| "0#{num}" }
+    end.join
   end
 end
 
@@ -9,3 +21,4 @@ class EncryptedSecret
     @ciphertext = ciphertext
   end
 end
+
--- part_04/spec/cipher_spec.rb
+++ part_04/spec/cipher_spec.rb
@@ -5,6 +5,53 @@ describe 'Secret' do
     Kernel.const_defined?('Secret')
     test = Secret.new "Testing, 1... 2... 3..!"
   end
+
+  describe '#encrypt' do
+    context 'When a new instance of Secret is created' do
+      it 'should exist as a method of the Secret class and take one argument' do
+        Secret.new("Test").encrypt(42)
+      end
+    end
+
+    context 'When called on Secret.new("0")' do
+      it 'should return "43" when passed 45' do
+        expect(Secret.new('0').encrypt(45)).to eq '43'
+      end
+    end
+
+    context 'When called on Secret.new("Look over there!")' do
+      it 'should return the right answer when passed 2374' do
+        expect(Secret.new('Look over there!').encrypt(2374)).to eq '37141410981421041798190704170452'
+      end
+
+      it 'should return the right answer when passed 2473' do
+        expect(Secret.new('Look over there!').encrypt(2473)).to eq '37141410981421041798190704170452'
+      end
+
+      it 'should return the right answer when passed 2572' do
+        expect(Secret.new('Look over there!').encrypt(2572)).to eq '37141410981421041798190704170452'
+      end
+    end
+
+    context 'When called on Secret.new("HELLO, 28 $$$!")' do
+      it 'should return the right answer when passed 7' do
+        expect(Secret.new('HELLO, 28 $$$!').encrypt(7)).to eq '4239464649850896030864646461'
+      end
+    end
+
+    context 'When called on Secret.new("       ")' do
+      it 'should return the right answer when passed 20' do
+        expect(Secret.new('       ').encrypt(20)).to eq '21212121212121'
+      end
+    end
+
+    context "When called on Secret.new('\' a double quote can be tricky, as can be \\ backslashes)" do
+      it 'should return the right answer when passed 43' do
+        expect(Secret.new("'\"' a double quote can be tricky, as can be \\ backslashes")
+        .encrypt(43)).to eq '181918444544485965465649446165596449444745584446494464625347556922444563444745584446494420444645475563564563524963'
+      end
+    end
+  end
 end
 
 describe 'EncryptedSecret' do
