d7325f735144579012be5973db05f90b50dff688
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-13 17:44:08 +0100

    Part 4: Refactored encrypt method tests to use local vars for readability

--- part_04/spec/cipher_spec.rb
+++ part_04/spec/cipher_spec.rb
@@ -9,46 +9,55 @@ describe 'Secret' do
   describe '#encrypt' do
     context 'When a new instance of Secret is created' do
       it 'should exist as a method of the Secret class and take one argument' do
-        Secret.new("Test").encrypt(42)
+        Secret.new('Test').encrypt(42)
       end
     end
 
     context 'When called on Secret.new("0")' do
       it 'should return "43" when passed 45' do
-        expect(Secret.new('0').encrypt(45)).to eq '43'
+        plaintext, key, ciphertext = '0', 45, '43'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
     end
 
     context 'When called on Secret.new("Look over there!")' do
       it 'should return the right answer when passed 2374' do
-        expect(Secret.new('Look over there!').encrypt(2374)).to eq '37141410981421041798190704170452'
+        plaintext, key, ciphertext = 'Look over there!', 2374, '37141410981421041798190704170452'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
 
       it 'should return the right answer when passed 2473' do
-        expect(Secret.new('Look over there!').encrypt(2473)).to eq '37141410981421041798190704170452'
+        plaintext, key, ciphertext = 'Look over there!', 2473, '37141410981421041798190704170452'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
 
       it 'should return the right answer when passed 2572' do
-        expect(Secret.new('Look over there!').encrypt(2572)).to eq '37141410981421041798190704170452'
+        plaintext, key, ciphertext = 'Look over there!', 2572, '37141410981421041798190704170452'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
     end
 
     context 'When called on Secret.new("HELLO, 28 $$$!")' do
       it 'should return the right answer when passed 7' do
-        expect(Secret.new('HELLO, 28 $$$!').encrypt(7)).to eq '4239464649850896030864646461'
+        plaintext, key, ciphertext = 'HELLO, 28 $$$!', 7, '4239464649850896030864646461'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
+
       end
     end
 
     context 'When called on Secret.new("       ")' do
       it 'should return the right answer when passed 20' do
-        expect(Secret.new('       ').encrypt(20)).to eq '21212121212121'
+        plaintext, key, ciphertext = '       ', 20, '21212121212121'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
     end
 
     context "When called on Secret.new('\' a double quote can be tricky, as can be \\ backslashes)" do
       it 'should return the right answer when passed 43' do
-        expect(Secret.new("'\"' a double quote can be tricky, as can be \\ backslashes")
-        .encrypt(43)).to eq '181918444544485965465649446165596449444745584446494464625347556922444563444745584446494420444645475563564563524963'
+        plaintext = "'\"' a double quote can be tricky, as can be \\ backslashes"
+        key = 43
+        ciphertext = '181918444544485965465649446165596449444745584446494464625347556922444563444745584446494420444645475563564563524963'
+        expect(Secret.new(plaintext).encrypt(key)).to eq ciphertext
       end
     end
   end
